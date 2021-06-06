2e356ff13079467f12163425e354750b7680f302
Chris Paul <cmpaul@fastmail.com>
2021-06-06 11:11:20 -0700

    Remove unnecessary files

--- ws/disconnect/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * used to clean up event.requestContext.connectionId
- */
-exports.handler = async function ws(event) {
-  console.log('ws-disconnect called with', event)
-  return {statusCode: 200}
-}
