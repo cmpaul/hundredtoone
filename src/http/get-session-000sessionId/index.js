ae12362ea05f6d9c2a69a9cedcca92220736c77f
Chris Paul <cmpaul@fastmail.com>
2021-06-06 11:25:14 -0700

    Fix websocket URL to reference env variable when deployed

--- src/http/get-session-000sessionId/index.js
+++ src/http/get-session-000sessionId/index.js
@@ -1,25 +1,22 @@
-let arc = require("@architect/functions");
-let static = arc.http.helpers.static;
+const arc = require("@architect/functions");
+const staticHelper = arc.http.helpers.static;
 
 function getWsUrl() {
-  let env = process.env.NODE_ENV
-  let testing = 'ws://localhost:3333'
-  let staging = 'fixme: these urls are printed after create'
-  let production = 'fixme: these urls are printed after create'
-  if (env === 'testing')
-    return testing
-  if (env === 'staging')
-    return staging
-  if (env === 'production')
-    return production
-  return testing
+  const env = process.env.NODE_ENV
+  const testing = 'ws://localhost:3333'
+  const deployed = process.env.ARC_WSS_URL;
+  const production = 'fixme: these urls are printed after create'
+  if (env === 'testing' || env === 'development') {
+    return testing;
+  }
+  return deployed;
 }
 
 exports.handler = async function http (req) {
   const sessionId = req.pathParameters['sessionId'];
   
   console.log(`GET /session/:sessionId called with sessionId ${sessionId}`);
-  
+
   return {
     statusCode: 200,
     headers: { 'content-type': 'text/html; charset=utf8' },
@@ -33,15 +30,15 @@ exports.handler = async function http (req) {
         <title>hundredto.one</title>
       
         <link rel='icon' type='image/png' href='/favicon.png'>
-        <link rel='stylesheet' href='${static('/global.css')}'>
-        <link rel='stylesheet' href='${static('/build/bundle.css')}'>
+        <link rel='stylesheet' href='${staticHelper('/global.css')}'>
+        <link rel='stylesheet' href='${staticHelper('/build/bundle.css')}'>
         <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x' crossorigin='anonymous'>
         <script type='text/javascript'>
           window.SESSION_ID = '${sessionId}';
           window.WS_URL = '${getWsUrl()}';
         </script>
       
-        <script defer src='${static('/build/bundle.js')}'></script>
+        <script defer src='${staticHelper('/build/bundle.js')}'></script>
         <script defer src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js' integrity='sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4' crossorigin='anonymous'></script>
       </head>
       
