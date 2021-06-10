88f77611c63f040e89f1607b5dfb1eaca7603dbf
Chris Paul <cmpaul@fastmail.com>
2021-06-09 19:20:23 -0700

    Add IdeaScroller

--- src/js/components/Brainstorm.svelte
+++ src/js/components/Brainstorm.svelte
@@ -3,25 +3,37 @@
   export let title;
   export let wsUrl;
 
-  import { Col, Row } from 'sveltestrap';
+  import { Container, Col, Row, InputGroup, Input, Button } from 'sveltestrap';
+  import IdeaScroller from './IdeaScroller.svelte';
 
-  let value = '';
+  let ideas = [];
   const ws = new WebSocket(wsUrl);
   ws.onopen = () => {
-    value += `${new Date(Date.now()).toISOString()} - opened\n`;
+    console.log('websocket opened')
     ws.send(JSON.stringify({ getHistory: true }));
   }
   ws.onclose = () => {
-    value += `${new Date(Date.now()).toISOString()} - closed\n`;
+    console.log('websocket closed');
   }
   ws.onmessage = (e) => {
-    const {history, text} = JSON.parse(e.data);
+    const {history, ...idea} = JSON.parse(e.data);
+    let ideasToAdd;
     if (history) {
+      if (!Array.isArray(history)) {
+        throw new Error('Invalid history type');
+      }
       if (history.length === 0) return;
-      value += history.map(i => i.text).join('\n') + '\n';
+      ideasToAdd = history;
     } else {
-      value += `${text}\n`;
+      ideasToAdd = [idea];
     }
+    // Svelte's reactivity is triggered by assignment, which is why
+    // we don't do a push here instead.
+    ideas = [...ideas, ...ideasToAdd].sort((a, b) => {
+      if (a.createdAt < b.createdAt) return -1;
+      if (a.createdAt > b.createdAt) return 1;
+      return 0;
+    });
   }
   ws.onerror = console.log
   const onkeyup = (e) => {
@@ -32,35 +44,38 @@
   }
 </script>
 
-<div class="container">
-  <Row>
-    <Col>
-      <h3>{title === null ? `Brainstorm ${id}` : title}</h3>
-    </Col>
-  </Row>
-  <Row>
-    <Col>
-      <input on:keyup|preventDefault={onkeyup} placeholder="Type a message and hit Enter..." />
-    </Col>
-  </Row>
-  <Row>
-    <Col>
-      <textarea bind:value/>
-    </Col>
-  </Row>
+<div>
+  <h3 class="float-right">{ideas.length}</h3>
+  <Container fluid>
+    <Row>
+      <Col>
+        <h1>{title === null ? `Brainstorm ${id}` : title}</h1>
+      </Col>
+    </Row>
+    <InputGroup>
+      <Input on:keyup={onkeyup} />
+      <Button size="sm" color="primary">Add</Button>
+    </InputGroup>
+    <Row>
+      <Col>
+        <IdeaScroller {ideas} />
+      </Col>
+    </Row>
+  </Container>
+  <div class="footer">
+    <div class="d-grid gap-2">
+      <Button class="end float-right" color="danger">End</Button>
+    </div>
+  </div> 
 </div>
 
 <style>
-  input {
+  .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
     width: 100%;
-    margin-bottom: 20px;
-  }
-  textarea {
-    width: 100%;
-    height: 600px;
-    font-family:'Courier New', Courier, monospace;
-  }
-  .container {
-    margin-top: 40px;
+    color: white;
+    text-align: center;
   }
 </style>
---
+++ src/js/components/IdeaScroller.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let ideas;
+
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    console.log('ideas', ideas);
+  });
+</script>
+
+<div class="container">
+  {#each ideas.reverse() as idea}
+    <p>{idea.text}</p>
+  {/each}
+</div>
